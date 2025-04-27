dcc5e30851150111fb84a2d64d95fe4a9c09c24f
Rajat Soni <rajatsonibkn2002@gmail.com>
2025-04-27 20:14:22 +0530

    added UI changes and toast

--- src/abi/config.ts
+++ src/abi/config.ts
@@ -50,7 +50,7 @@ export const CONTRACTS: Record<
     evilAddress: "0xf16834786221C51678cB386cf77a57ac7fcb3441",
     treasury: "0x1A530A9803B8faFA34491D70Fa7EE49C220e29Dc",
     jackpot: "0xf883c46b6Ac7d7b8936122F88b75070Ad6f2f47B",
-    pinkMistWell: "0x2629837B7dbb1bdFe7ab7F04F1297cF8DE00609C",
+    pinkMistWell: "0x35BAFE8D9f92BbA6a6fbBDC85d9fd4bF74E6852e",
   },
   [baseSepolia.id]: {
     miningRig: "0x8Dea737AE483153c69934ff8a5c7E3D448c2DB4C",
--- src/app/(game)/Components/PMWTitle.tsx
+++ src/app/(game)/Components/PMWTitle.tsx
@@ -1,13 +1,19 @@
 import { IMAGEKIT_LOGOS } from "@/assets/imageKit";
 import Image from "next/image";
-export default function PMWTitle() {
+
+interface PMWTitleProps {
+  width?: number;
+}
+
+export default function PMWTitle({ width = 60 }: PMWTitleProps) {
   return (
     <Image
       src={IMAGEKIT_LOGOS.PINK_MIST_WHALE_LOGO}
       alt="Pink Mist Whale"
-      className="w-[60%] mb-5"
+      className="mb-5"
       width={100}
       height={100}
+      style={{ width: `${width}%` }}
     />
   );
 }
