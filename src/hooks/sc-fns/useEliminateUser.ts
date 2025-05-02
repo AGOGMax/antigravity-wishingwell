82471d9bca065c48e5e5ff81f82b5108168a9fc7
Anushka <anushkavarshney2708@gmail.com>
2025-05-03 02:12:25 +0530

    integgrated api to get calldata value

--- src/hooks/sc-fns/useEliminateUser.ts
+++ src/hooks/sc-fns/useEliminateUser.ts
@@ -1,12 +1,14 @@
 import { useEffect, useState } from "react";
 import {
-  useAccount,
   useReadContracts,
   useWaitForTransactionReceipt,
   useWriteContract,
 } from "wagmi";
 import usePinkMistWellContract from "@/abi/PinkMistWell";
 import { ToastOptions, toast } from "react-hot-toast";
+import axios from "axios";
+import { API_ENDPOINT } from "@/constants";
+
 
 const TOAST_SETTINGS: ToastOptions = {
   duration: 3000,
@@ -15,6 +17,21 @@ const TOAST_SETTINGS: ToastOptions = {
     width: "400px",
   },
 };
+
+
+async function getCallbackValue() {
+  try {
+    const response = await axios.get("https://test-api.agproject.xyz/api/calldata");
+    const data = response.data;
+    const callData = JSON.parse(data).calldata;
+    console.log("api response", (callData));
+    return callData;
+  } catch (error) {
+    console.error("error", error);
+  }
+}
+
+getCallbackValue();
 
 const useEliminateUser = () => {
   const [transactionLoading, setTransactionLoading] = useState<boolean>(false);
@@ -87,12 +104,12 @@ const useEliminateUser = () => {
   const eliminateUser = (ticketCount = 1) => {
     if (eliminationTokenAddress && typeof eliminationFee === "bigint") {
       setTransactionLoading(true);
-
+      const callData = getCallbackValue()
       eliminateUserFn({
         address: PMWContract?.address as `0x${string}`,
         abi: PMWContract?.abi,
         functionName: "eliminateTicket",
-        args: [BigInt(ticketCount)],
+        args: [BigInt(ticketCount),callData ], //bytes data
         value: eliminationFee as bigint,
       });
     }
