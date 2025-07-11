1b487a20234ef905b015c8df7b319e632fe25a47
Rajat Soni <rajatsonibkn2002@gmail.com>
2025-07-11 21:01:41 +0530

    added test log

--- src/hooks/core/useLottery.ts
+++ src/hooks/core/useLottery.ts
@@ -363,6 +363,12 @@ const useLottery = (): {
           total: proofs.length,
         });
 
+        console.log(
+          "debug, jackpot address",
+          JackpotContract.address,
+          JackpotContract,
+        );
+
         const tx = await batchPruneWinnings({
           address: JackpotContract.address as `0x${string}`,
           abi: JackpotContract.abi,
