286655de30f4dfc772a76791f00bee637674aeee
AGOGMax <hextips@gmail.com>
2026-01-12 01:19:49 -0700

    Update MintingCalculator.tsx

--- src/components/Minting/MintingCalculator.tsx
+++ src/components/Minting/MintingCalculator.tsx
@@ -2,7 +2,6 @@
 
 import { MAX_INPUT, getCurrentBuyAnimation } from "./utils"; 
 // Note: If your file is actually named 'utils.ts' with an 's', use "./utils" instead.
-
 import { twMerge } from "tailwind-merge";
 import {
   Dispatch,
