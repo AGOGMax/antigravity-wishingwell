821b59248abb71435804072e4cf73dd4554117a9
AGOGMax <hextips@gmail.com>
2026-01-11 20:44:00 -0700

    Fix import path for useCountdownTimer

--- src/hooks/frontend/useTimer.ts
+++ src/hooks/frontend/useTimer.ts
@@ -3,7 +3,7 @@ import { useReadContracts } from "wagmi";
 import { pulsechain, pulsechainV4 } from "viem/chains";
 import { TEST_NETWORK } from "@/constants";
 import useJPMContract from "@/abi/JourneyPhaseManager";
-import useCountdownTimer from "./useCountdownTimer";
+import useCountdownTimer from "../useCountdownTimer";
 
 // This interface now perfectly matches what CountdownTimer.tsx expects
 export interface CountdownType {
